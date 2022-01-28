4ce5e92b12fadb726a6a91893e2843b82ac13665
Francis Alonzo <44241055+supertoto29@users.noreply.github.com>
2022-01-28 12:35:24 -0500

    push last modif

--- src/main.cpp
+++ src/main.cpp
@@ -24,35 +24,40 @@ int main()
   chip_select = 1;
   INA229 device(&spi, &chip_select);
   spi.format(8, 1); // Mode 1
-  spi.frequency(1000000);
+  spi.frequency(500000);
 
-  device.setConfig(CONFIG_SET);
-  if(device.getConfig() == CONFIG_SET) (led2.read()) ? led2 = 0 : led2 = 1;
+  uint16_t tmp = device.getID();
+  if(tmp != 0) (led2.read()) ? led2 = 0 : led2 = 1;
+  tmp = device.getManufacturer();
+  if(tmp != 0) (led2.read()) ? led2 = 0 : led2 = 1;
+
+  // device.setConfig(CONFIG_SET);
+  // if(device.getConfig() == CONFIG_SET) (led2.read()) ? led2 = 0 : led2 = 1;
   
-  ThisThread::sleep_for(1000);  
+  // ThisThread::sleep_for(1000);  
 
-  device.setConfigADC(CONFIG_ADC_SET);
-  if(device.getConfigADC() == CONFIG_ADC_SET) (led2.read()) ? led2 = 0 : led2 = 1;
+  // device.setConfigADC(CONFIG_ADC_SET);
+  // if(device.getConfigADC() == CONFIG_ADC_SET) (led2.read()) ? led2 = 0 : led2 = 1;
 
-  ThisThread::sleep_for(1000);
+  // ThisThread::sleep_for(1000);
 
-  device.setShuntCal(SHUNT_CALIBRATION);
-  if(device.getShuntCal() == SHUNT_CALIBRATION) (led2.read()) ? led2 = 0 : led2 = 1;
+  // device.setShuntCal(SHUNT_CALIBRATION);
+  // if(device.getShuntCal() == SHUNT_CALIBRATION) (led2.read()) ? led2 = 0 : led2 = 1;
 
-  ThisThread::sleep_for(1000);
+  // ThisThread::sleep_for(1000);
 
-  device.setBOVL(OVERVOLTAGE_BUS_THRESHOLD);
-  if(device.getBOVL() == OVERVOLTAGE_BUS_THRESHOLD) (led2.read()) ? led2 = 0 : led2 = 1;
+  // device.setBOVL(OVERVOLTAGE_BUS_THRESHOLD);
+  // if(device.getBOVL() == OVERVOLTAGE_BUS_THRESHOLD) (led2.read()) ? led2 = 0 : led2 = 1;
 
-  ThisThread::sleep_for(1000);
+  // ThisThread::sleep_for(1000);
 
-  device.setBUVL(UNDERVOLTAGE_BUS_THRESHOLD);
-  if(device.getBUVL() == UNDERVOLTAGE_BUS_THRESHOLD) (led2.read()) ? led2 = 0 : led2 = 1;
+  // device.setBUVL(UNDERVOLTAGE_BUS_THRESHOLD);
+  // if(device.getBUVL() == UNDERVOLTAGE_BUS_THRESHOLD) (led2.read()) ? led2 = 0 : led2 = 1;
 
-  ThisThread::sleep_for(1000);
+  // ThisThread::sleep_for(1000);
 
-  device.setOverTempLimit(OVERTEMPERATURE_THRESHOLD);
-  if(device.getOverTempLimit() == OVERTEMPERATURE_THRESHOLD) (led2.read()) ? led2 = 0 : led2 = 1;
+  // device.setOverTempLimit(OVERTEMPERATURE_THRESHOLD);
+  // if(device.getOverTempLimit() == OVERTEMPERATURE_THRESHOLD) (led2.read()) ? led2 = 0 : led2 = 1;
 
   ThisThread::sleep_for(1000);
 
