f4478fe51e4a1d5971df89b5183e6c03766fa9de
Francis Alonzo <44241055+supertoto29@users.noreply.github.com>
2022-05-20 17:40:00 -0400

    first test to complete

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 
 #include "main.h"
 
+Serial pc(USBTX, USBRX, "debug", 115200);
+
 int main()
 {
   // Init l'ESC
@@ -20,13 +22,13 @@ int main()
   in = 1;
   ThisThread::sleep_for(5000);
 
-  // // Test #1 : Puissance maximale compé durant 10 mins
-  // pwm.pulsewidth_us(1800);
-  // for (uint8_t i = 0; i < 10; ++i)
-  // {
-  //   ThisThread::sleep_for(60000); // Sleep 60 secondes
-  //   led3 = !(led3);
-  // }
+  // Test #1 : Puissance maximale compé durant 10 mins sans cooling
+  pwm.pulsewidth_us(1700);
+  for (uint8_t i = 0; i < 1; ++i)
+  {
+    ThisThread::sleep_for(2000); // Sleep 60 secondes
+    led3 = !(led3);
+  }
 
   // // Test #2 : Puissance maximale moteur 30 sec ON / 30 sec OFF
   // for (uint8_t i = 0; i < 10; ++i)
@@ -69,25 +71,37 @@ int main()
   //   pwm_send += 50;
   // }
 
-  // // Test #5 : Valeur température finale pour la puissance observée
-  // pwm.pulsewidth_us(1900);
+  // // Test #5 : Valeur température finale pour la puissance désirée
+  // pwm.pulsewidth_us(1700); // Mettre le PWM souhaité
+  // led3 = 1;
   // for (uint8_t i = 0; i < 10; ++i) // Ne devrait pas être en mesure de finir avant la fin
   // {
-  //   ThisThread::sleep_for(60000); // Sleep 1 minute  
+  //   ThisThread::sleep_for(60000); // Sleep 1 minute
   // }
 
-  // Test #6 : Test pour éteindre avec un PWM
-  pwm.pulsewidth_us(1900);
-  ThisThread::sleep_for(5000);
-  in = 0;
-  ThisThread::sleep_for(5000);
-
-  // // Test #7 : Puissance maximale moteur - Test destructif
-  // pwm.pulsewidth_us(1900);
-  // for (uint8_t i = 0; i < 10; ++i) // Ne devrait pas être en mesure de finir avant la fin
+  // Test #6 : Test pour éteindre avec un PWM actif sur la pin INPUT
+  // int32_t delay = 1000; // ms
+  // while(delay >= 1)
   // {
-  //   ThisThread::sleep_for(60000); // Sleep 1 minute  
+  //   printf("Delay : %d\n", delay);
+  //   led3 = 1;
+  //   pwm.pulsewidth_us(1900);
+  //   ThisThread::sleep_for(2000);
+  //   in = 0;
+  //   ThisThread::sleep_for(delay);
+  //   in = 1;
+  //   delay -= 100;
+  //   ThisThread::sleep_for(2000);
+  //   led3 = 0;
+  //   in = 0;
+  //   ThisThread::sleep_for(2000);
+
+  //   // Reset the PWM / ESC for next test
+  //   pwm.period_us(NEUTRAL_PWM);
+  //   in = 1;
+  //   ThisThread::sleep_for(5000);
   // }
+
 
   // Test terminé
   pwm.pulsewidth_us(NEUTRAL_PWM);
